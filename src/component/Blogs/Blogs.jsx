2cbea5c19240b2128dde3183ab29f6c5a53438d0
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-03-31 03:31:04 +0600

    data loaded dynamically in blogs section

--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const Blogs = () => {
+const Blogs = ({ blogs }) => {
+    console.log(blogs);
+    const { id, title, author, datePosted, readTime, tags, coverImg, authorImg } = blogs;
+
     return (
         <div>
-            <p>Blogs Section</p>
+            <div>
+                <img src={coverImg} alt="cover photo" />
+            </div>
+            <div>
+                <div>
+                    <img src={authorImg} alt="author photo" />
+                    <div>
+                        <h3>{author}</h3>
+                        <p>{datePosted}</p>
+                    </div>
+                </div>
+                <p>{readTime} min read</p>
+            </div>
+            <div>
+                <h2>{title}</h2>
+                {
+                    tags.map((tag, index) => <p key={index}>{tag}</p>)
+                }
+                <p>Mark as read</p>
+            </div>
         </div>
     );
 };
--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Blogs from '../Blogs/Blogs';
 import Action from '../Actions/Action';
+import { data } from 'autoprefixer';
 
 const Main = () => {
+    const [blogs, setBlogs] = useState([]);
+    useState(() => {
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
+
     return (
         <div className='grid grid-cols-3 my-8'>
             <div className='col-span-2'>
-                <Blogs></Blogs>
+                {
+                    blogs.map(blog => <Blogs
+                        blogs={blog}
+                        key={blog.id}
+                    ></Blogs>)
+                }
             </div>
             <div>
                 <Action></Action>
